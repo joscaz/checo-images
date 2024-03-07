7b96cf9082031f44fe285a0bc72a66554432335a
joscaz <joscazertuche@gmail.com>
2024-03-06 22:38:12 -0600

    Project configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,23 @@
 from flask import Flask
 from flask_cors import CORS
+import logging
 
 flask_app = Flask(__name__)
+CORS(flask_app)
+
+
+flask_app.config.from_object('config')
+
+
+logging.getLogger().setLevel(logging.INFO)
+flask_app.logger.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+flask_app.logger.addHandler(handler)
 
 @flask_app.route("/")
 def hello_world():
     return "<p>Helo, world!</p>"
+
+import app.routes
--- config.py
+++ config.py
@@ -6,3 +6,6 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+
+BUCKET_NAME = os.environ.get('BUCKET_NAME')
+IMAGE_PREFIX = os.environ.get('IMAGE_PREFIX')
