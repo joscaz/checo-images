78d2c2d21cfd5087322de9afeaa2d03f20364a16
joscaz <joscazertuche@gmail.com>
2024-03-06 22:37:57 -0600

    Routes of the API

---
+++ app/routes.py
@@ -0,0 +1,9 @@
+from app import flask_app
+from app.s3_manager import get_random_image
+
+@flask_app.route("/checo", methods=['GET'])
+def get_random_checo_image():
+    return get_random_image()
+
+if __name__ == '__main__':
+    flask_app.run(debug=True)
