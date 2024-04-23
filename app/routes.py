4ec6c98a274b2ac055446ffbd6708e9e9d71b11a
joscaz <joscazertuche@gmail.com>
2024-04-23 13:10:59 -0600

    delete run from routes

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,3 @@ from app.s3_manager import get_random_image
 @flask_app.route("/checo", methods=['GET'])
 def get_random_checo_image():
     return get_random_image()
-
-if __name__ == '__main__':
-    flask_app.run(debug=False)
